da921e9051251b13bd8c2f810cfd09ba8d547232
vigneshrmdec <uee14330@rmd.ac.in>
2017-02-02 18:00:59 +0530

    Create array2.c

---
+++ array2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a[10],b[10],i,j=0,num,search;
+printf("enter the size:");
+scanf("%d",&num);
+printf("enter the any 5 no:");
+for(i=0;i<num;i++)
+ {
+   scanf("%d",&a[i]);
+ }
+printf("\nenter the search element:");
+scanf("%d",&search);
+for(i=0;i<num;i++)
+  {
+    if(a[i]==search)
+      {
+        b[j]=a[i];
+        j++;
+      }
+  }
+printf("output:");
+for(i=0;i<j;i++)
+  printf("%d",b[i]);
+return(0);
+}
